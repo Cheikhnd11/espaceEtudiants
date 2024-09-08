0a0abbdf77a74358783a804318b6fb3eb99940f9
Cheikhnd11 <cn7061611@gmail.com>
2024-09-08 01:20:07 +0000

    Initial commit

---
+++ src/main/java/booles/teame/espaceEtudiants/EspaceEtudiantsApplication.java
@@ -0,0 +1,13 @@
+package booles.teame.espaceEtudiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspaceEtudiantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspaceEtudiantsApplication.class, args);
+	}
+
+}
